7c0f84560d50aa39b890e090ceae96c3e11f116f
cleanown <1346387492@qq.com>
2020-04-27 02:11:22 +0800

    1

--- pages/index/index.js
+++ pages/index/index.js
@@ -5,15 +5,53 @@ const app = getApp()
 Page({
   data: {
     inputShowed: false,
-    inputVal: ""
+    inputVal: "",
+    companylist: [{
+      companyname: '广州千锋有限责任公司',
+      province: '广州市'
+    }, {
+      companyname: '广州千锋有限责任公司',
+      province: '广州市'
+    }, {
+      companyname: '广州千锋有限责任公司',
+      province: '广州市'
+    }, {
+      companyname: '广州千锋有限责任公司',
+      province: '广州市'
+    }, {
+      companyname: '广州千锋有限责任公司',
+      province: '广州市'
+    }, {
+      companyname: '广州千锋有限责任公司',
+      province: '广州市'
+    }, {
+      companyname: '广州千锋有限责任公司',
+      province: '广州市'
+    }, {
+      companyname: '广州千锋有限责任公司',
+      province: '广州市'
+    }]
   },
   onLoad() {
     this.setData({
       search: this.search.bind(this)
     })
   },
+  // onShow: function () {
+  //   this.getCompanylsit()
+  // },
+  // getCompanylsit: function () {
+  //   wx.request({
+  //     url: 'http://api.cleanown.cn/search/companylist',
+  //     method: "GET",
+  //     success(res) {
+  //       console.log(res)
+  //     }
+  //   })
+  // },
   search: function (value) {
-    return new Promise((resolve, reject) => {
+      console.log(value)
+      return new Promise((resolve, reject) => {
       setTimeout(() => {
         resolve([{ text: '搜索结果', value: 1 }, { text: '搜索结果2', value: 2 }])
       }, 200)
