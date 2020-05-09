a1473878d7b25bb5e95d9da0d2b57bfc4c8caaba
cleanown <1346387492@qq.com>
2020-05-09 19:38:35 +0800

    1

--- pages/article/article.js
+++ pages/article/article.js
@@ -8,9 +8,60 @@ Page({
    */
   data: {
     company: {},
-    releaseTime: ''
+    releaseTime: '',
+    starcolor: 'black',
+    startype: 'outline',
+    likecolor: 'black',
+    liketype: 'outline',
+    likenum: 55,
+    inputshow: false
   },
-
+  star: function () {
+    if (this.data.starcolor === 'black') {
+      wx.showToast({
+        title: '收藏成功',
+      })
+    } else {
+      wx.showToast({
+        title: '取消收藏',
+      })
+    }
+    this.setData ({
+      starcolor: this.data.starcolor === 'black' ? '#651FFF' : 'black',
+      startype: this.data.startype === 'outline' ? 'field' : 'outline'
+    })
+  },
+  like: function () {
+    if (this.data.likecolor === 'black') {
+      wx.showToast({
+        title: '点赞成功',
+      })
+      this.setData({
+        likenum: this.data.likenum += 1
+      })
+    } else {
+      wx.showToast({
+        title: '取消点赞',
+      })
+      this.setData({
+        likenum: this.data.likenum -= 1
+      })
+    }
+    this.setData ({
+      likecolor: this.data.likecolor === 'black' ? '#651FFF' : 'black',
+      liketype: this.data.liketype === 'outline' ? 'field' : 'outline'
+    })
+  },
+  comment: function () {
+    this.setData({
+      inputshow: true
+    })
+  },
+  inputClose: function () {
+    this.setData({
+      inputshow: false
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
